ebc0e2018d6b81ebe49bb9817b4faaf784fb3799
Eyal Toledano <eyal@microangel.so>
2025-03-25 00:12:29 -0400

    fix: camelCase detection mechanism in global CLI

--- scripts/dev.js
+++ scripts/dev.js
@@ -8,6 +8,11 @@
  * It imports functionality from the modules directory and provides a CLI.
  */
 
+// Add at the very beginning of the file
+if (process.env.DEBUG === '1') {
+  console.error('DEBUG - dev.js received args:', process.argv.slice(2));
+}
+
 import { runCLI } from './modules/commands.js';
 
 // Run the CLI with the process arguments
